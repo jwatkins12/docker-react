e88de4f96a6f34b631b9764c82d6f6915771a2f4
Joseph Watkins <josephwatkins@OCS301-MacBook-Pro.local>
2021-07-28 14:43:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pizza is the best
         </p>
         <a
           className="App-link"
